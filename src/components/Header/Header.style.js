3747d8efb3501a75f38b58f0e352c606a7b36829
Yuru Huang <ruby_huang@gmx.ch>
2023-02-27 15:54:29 +0100

    remove styled component

--- src/components/Header/Header.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledHeader = styled.header`
-    color: black;
-    background-color: yellow;
-    display: flex;
-    justify-content: space-between
-`
-
