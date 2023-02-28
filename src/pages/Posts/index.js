c73aeb721a7dae6fe169aa058ef04635b3eac605
juangallego17 <juangallego17@hotmail.com>
2023-02-28 18:42:33 +0100

    created the post page with fetches. Missing layout

--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,6 +1,7 @@
 import Header from "../../components/Header";
-import { MainTop } from "./Components/Subcomponents/MainTop.style";
-import { NavLink, Outlet } from "react-router-dom";
+
+import { Outlet } from "react-router-dom";
+import MainTop from "./Components/MainTop/MainTop";
 
 
 
@@ -19,27 +20,7 @@ const Posts = () => {
     return (
         <>
          <Header />
-         <MainTop className="mainTop">
-            <div className="navigation">
-            <div className="leftNav">
-                <div className="searchLogo">
-                <img src="images/svg/search_icon.svg" alt="" />
-                </div>
-                <div className="searchText">Search posts...</div>
-            </div>
-            <div className="rightNav">
-                <div className="liked">
-                <div><NavLink to={"/posts/liked"}>Liked</NavLink></div>
-                </div>
-                <div className="friends">
-                <div><NavLink to={"/posts/friends"}>Friends</NavLink></div>
-                </div>
-                <div className="follows">
-                <div><NavLink to={"/posts/follow"}>Follow</NavLink></div>
-                </div>
-            </div>
-            </div>
-        </MainTop>
+        <MainTop/>
 
         <Outlet/>
 
