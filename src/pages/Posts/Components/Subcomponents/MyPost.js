45f38d8c38bb2283c7e23dddecaa26a3cd0655bb
juangallego17 <juangallego17@hotmail.com>
2023-02-28 18:05:16 +0100

    initial commit

---
+++ src/pages/Posts/Components/Subcomponents/MyPost.js
@@ -0,0 +1,44 @@
+
+
+const MyPost = (props) => {
+
+
+
+
+return(
+    <div>{props.post}</div>
+)
+
+}
+
+export default MyPost
+
+
+
+
+{/* <div className="post1 post">
+                <div className="headerPost"></div>
+                <div className="textPost">
+                    Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas
+                    laudem iracundia et, ad per utamur ceteros apeirian…
+                </div>
+                <div className="picturesPost"></div>
+                <div className="footerPost"></div>
+                </div>
+                <div className="post2 post">
+                <div className="headerPost"></div>
+                <div className="textPost">
+                Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas
+                laudem iracundia et, ad per utamur ceteros apeirian…
+                </div>
+                <div className="footerPost"></div>
+                </div>
+                <div className="repost post">
+                <div className="headerPost"></div>
+                <div className="textPost">
+                    Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas
+                    laudem iracundia et, ad per utamur ceteros apeirian…
+                </div>
+                <div className="repostPost"></div>
+                <div className="footerPost"></div>
+                </div> */}
--- src/pages/Posts/index.js
+++ src/pages/Posts/index.js
@@ -1,9 +1,49 @@
 import Header from "../../components/Header";
+import { MainTop } from "./Components/Subcomponents/MainTop.style";
+import { NavLink, Outlet } from "react-router-dom";
+
+
+
+
+
+
 const Posts = () => {
+
+
+
+
+
+
+    
+
     return (
         <>
          <Header />
-         <span>Posts</span>
+         <MainTop className="mainTop">
+            <div className="navigation">
+            <div className="leftNav">
+                <div className="searchLogo">
+                <img src="images/svg/search_icon.svg" alt="" />
+                </div>
+                <div className="searchText">Search posts...</div>
+            </div>
+            <div className="rightNav">
+                <div className="liked">
+                <div><NavLink to={"/posts/liked"}>Liked</NavLink></div>
+                </div>
+                <div className="friends">
+                <div><NavLink to={"/posts/friends"}>Friends</NavLink></div>
+                </div>
+                <div className="follows">
+                <div><NavLink to={"/posts/follow"}>Follow</NavLink></div>
+                </div>
+            </div>
+            </div>
+        </MainTop>
+
+        <Outlet/>
+
+
         </>
     )
 }
