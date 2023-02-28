212e4ad97e89551926f3c15f0bc52558a83b6e14
Yuru Huang <ruby_huang@gmx.ch>
2023-02-28 10:46:07 +0100

    friends card layout and styled

--- src/pages/Friends/FriendsCard.js
+++ src/pages/Friends/FriendsCard.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import alber from '../../assets/images/users/alber.png'
 
 //-------STYLE--------->
+
 const StyledFriendCard = styled.div`
     margin-top: 120px;
     box-sizing: border-box;
@@ -10,30 +11,10 @@ const StyledFriendCard = styled.div`
     display: flex;  flex-direction: column;
     justify-content: flex-start; align-items: center;
     height: 489px; width: 362px;
-    border-radius: 4px;
+    border-radius: 5px;
     background-color: #FFF;
     gap: 30px;
-    .btn{
-        background-color: #fff;
-        margin: 0 3px;
-        width: 120px; height: 40px;
-        border-radius: 30px;
-        border: 1px solid rgba(0,0,0,.5);
-        font-size: 0.8rem; font-weight: 500;  
-        :hover{
-        background: linear-gradient(132.96deg, #C468FF 3.32%, #6E91F6 100%);
-        border: none;
-        color: #FFF;
-        } 
-    }
-    .user-hobbies{
-        width: 100%;
-        display:flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-
-    }
+  
 `;
 const Tag = styled.span`
         margin: 3px;
@@ -44,48 +25,65 @@ const Tag = styled.span`
         font-size: 1rem; font-weight: 400; 
 `;
 
-const UserInfo = styled.div`
+const FlexDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    text-align: center;
     gap: .3rem;
-    .user-pic{
-        width: 90px;
-    }
-    .user-name{
-        font-size: 1.5rem;
-        font-weight: 500;
-    }
-    .user-country{
-        font-size: .9rem;
-        font-weight: 400px;
-    }
+   .img{
+    width: 90px;
+   }
+   .name{
+    font-size: 22px;
+   }
 
 `;
-
+const HobbiesBox = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+`;
+const ButtonContainer = styled.div`
+    display: flex;
+    gap: 6px;
+`;
+const Button = styled.button`
+    width: 120px; height: 40px;
+    background-color: #fff;
+    border-radius: 30px;
+    border: 1px solid rgba(0,0,0,.5);
+    font-size: 0.8rem; font-weight: 500;  
+        :hover{
+        background: linear-gradient(132.96deg, #C468FF 3.32%, #6E91F6 100%);
+        border: none;
+        color: #FFF;
+        font-weight: 600;
+        } 
+`;
+//-------Component--------->
 export default function FriendsCard() {
   return (
     <StyledFriendCard >
-      <UserInfo>
-        <img className='user-pic' src={alber}/>
-        <p className='user-name'>Albert Lawrence</p>
-        <p className='user-country'>Zürich, Switzerland</p>
-      </UserInfo>
-      <div className='acion-buttons'>
-        <button className='btn'>FOLLOW</button>
-        <button className='btn'>ADD FRIEND</button>
-      </div>
-      <div className='user-about'>
+      <FlexDiv>
+        <img className='img' src={alber}/>
+        <p className='name'>Albert Lawrence</p>
+        <p className='country'>Zürich, Switzerland</p>
+      </FlexDiv>
+      <ButtonContainer>
+        <Button>FOLLOW</Button>
+        <Button>ADD FRIEND</Button>
+      </ButtonContainer>
+      <FlexDiv>
         <p>Lorem ipsum dolor sit amet, vim ut quas volumus probatus, has tantas laudem iracundia et, ad per utamur ceteros apeirian</p>
-      </div>
-      <div className='user-hobbies'>
+      </FlexDiv>
+      <HobbiesBox>
         <Tag>Hobby</Tag>
         <Tag>Hobby</Tag>
         <Tag>Hobby</Tag>
         <Tag>Hobby</Tag>
         <Tag>Hobby</Tag>
-       
-      </div>
+      </HobbiesBox>
     </StyledFriendCard>
   );
 }
